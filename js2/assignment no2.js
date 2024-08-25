e7a65819e8351504f692f2e99415b3a97f47b7d7
BAZIL UR REHMAN <bazilurrehman08@gmail.com>
2024-08-25 12:28:21 +0500

    Add files via upload

---
+++ js2/assignment no2.js
@@ -0,0 +1,12 @@
+// assignment mo1
+
+var age = 21
+alert(age)
+
+
+alert ("you have visited this site 14 times.")
+
+var birthyear = 2002
+document.write("<h1> my birth year is 2002  </br> My age is 21.</h1>")
+
+document.write(" <b> john doe </b>  ordrerd <b> 5 T-Shirts </b> on XYZ clothing store. ")
